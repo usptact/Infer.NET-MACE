776aae880bc2eddd954d47ec66726013491fbbe0
Vladislavs Dovgalecs <usptact@inbox.lv>
2017-08-04 00:26:13 -0700

    Added parameter definitions and their priors

--- MACE/Program.cs
+++ MACE/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using MicrosoftResearch.Infer;
 using MicrosoftResearch.Infer.Distributions;
 using MicrosoftResearch.Infer.Maths;
@@ -14,30 +15,42 @@ namespace MACE
             const int numItems = 50;
             const int numCategories = 3;
 
-            int[][] data = new int[numWorkers][];
+            int[][] data = new int[numItems][];
 
             Range n = new Range(numItems);
             Range m = new Range(numWorkers);
 
             var S = Variable.Array(Variable.Array<bool>(m), n);
             var A = Variable.Array(Variable.Array<int>(m), n);
+            A.ObservedValue = data;
+
+            Beta thetaPrior = new Beta(1, 1);
+            var theta = Variable.Array<double>(m);
+            theta[m] = Variable.Random(thetaPrior).ForEach(m);
+
+            double[] initCounts = Enumerable.Repeat<double>(1.0, numCategories).ToArray();
+            Dirichlet ksiPrior = new Dirichlet(initCounts);
+            var ksi = Variable.Array<Vector>(m);
+            ksi[m] = Variable.Random(ksiPrior).ForEach(m);
 
             using (Variable.ForEach(n))
             {
                 var T = Variable.DiscreteUniform(numCategories);
                 using (Variable.ForEach(m))
                 {
-                    S[n][m] = Variable.Bernoulli(1 - theta[j]);
+                    S[n][m] = Variable.Bernoulli(1 - theta[m]);
                     using (Variable.If(S[n][m]))
                     {
                         A[n][m] = T;
                     }
                     using (Variable.IfNot(S[n][m]))
                     {
-                        A[n][m] = Variable.Multinomial()
+                        A[n][m] = Variable.Discrete(ksi[m]);
                     }
                 }
             }
+
+            InferenceEngine engine = new InferenceEngine();
         }
     }
 }
