0938b5e11f9fd31f6efea94e014c50ac612397ec
Vladislavs Dovgalecs <vladislavsd@voicebox.com>
2017-08-03 18:01:39 -0700

    non-working sketch of the program

--- MACE/Program.cs
+++ MACE/Program.cs
@@ -10,6 +10,34 @@ namespace MACE
     {
         static void Main(string[] args)
         {
+            const int numWorkers = 10;
+            const int numItems = 50;
+            const int numCategories = 3;
+
+            int[][] data = new int[numWorkers][];
+
+            Range n = new Range(numItems);
+            Range m = new Range(numWorkers);
+
+            var S = Variable.Array(Variable.Array<bool>(m), n);
+            var A = Variable.Array(Variable.Array<int>(m), n);
+
+            using (Variable.ForEach(n))
+            {
+                var T = Variable.DiscreteUniform(numCategories);
+                using (Variable.ForEach(m))
+                {
+                    S[n][m] = Variable.Bernoulli(1 - theta[j]);
+                    using (Variable.If(S[n][m]))
+                    {
+                        A[n][m] = T;
+                    }
+                    using (Variable.IfNot(S[n][m]))
+                    {
+                        A[n][m] = Variable.Multinomial()
+                    }
+                }
+            }
         }
     }
 }
