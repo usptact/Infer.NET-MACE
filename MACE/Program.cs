78b254aba3cb6e9d03895192c9287dd6f2096519
Vladislavs Dovgalecs <vladislavsd@voicebox.com>
2017-08-03 17:38:54 -0700

    Add project files.

---
+++ MACE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MicrosoftResearch.Infer;
+using MicrosoftResearch.Infer.Distributions;
+using MicrosoftResearch.Infer.Maths;
+using MicrosoftResearch.Infer.Models;
+
+namespace MACE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
